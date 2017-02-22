346b5f53570fc1743b161e7e63c2b5f5536fc7b9
gold <mcavoto@sjnma.org>
2017-02-22 10:51:51 -0600

    [Finished the first problem]

---
+++ Problem1.py
@@ -0,0 +1,8 @@
+a=int(raw_input("Please give me an  integer." '>'))
+
+b=int(raw_input("Please give me another integer." '>'))
+
+c =int(raw_input("Please give me one more integer." '>'))
+
+if( a + b == c): 
+    print("true")
