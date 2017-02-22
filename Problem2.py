b416dc6bb2553e84d507c1de790c6924180a02f3
gold <mcavoto@sjnma.org>
2017-02-22 11:05:21 -0600

    [Finished Problem 2]

---
+++ Problem2.py
@@ -0,0 +1,15 @@
+a1= int(raw_input("Please give me a number" '>'))
+
+a2= int(raw_input("Please give me another number" '>'))
+
+a3= int(raw_input("Please give me one more number" '>'))
+
+if( a1< a2< a3):
+    print("Up and Rising")
+if( a1> a2> a3):
+    print("Falling Down")
+if( a1< a2 > a3):
+    print("Over the Mountain")
+if( a1> a2 < a3):
+    print("In the Dog House")
+
